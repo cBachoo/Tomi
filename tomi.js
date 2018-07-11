532a2cda528979f4644e19e8bca6ab67572aa0a6
Bachoo <cosmin_cb@hotmail.com>
2018-07-11 19:02:57 -0400

    changed tomi's status message for discord

--- tomi.js
+++ tomi.js
@@ -20,19 +20,19 @@ const helptxt = fs.readFileSync("./help.txt");
 client.on('ready', () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
     console.log(`Successfully logged in as ${client.user.tag}!`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers! <3`);
+    client.user.setActivity(`Use t!help! Serving ${client.guilds.size} servers! <3`);
 });
 
 //called when the bot joins a guild.
 client.on("guildCreate", guild => {
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers! <3`);
+    client.user.setActivity(`Use t!help! Serving ${client.guilds.size} servers! <3`);
 });
 
 //called when the bot is removed from a guild.
 client.on("guildDelete", guild => {
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers! <3`);
+    client.user.setActivity(`Use t!help! Serving ${client.guilds.size} servers! <3`);
 });
 
 
