57c8a2482f3f06a00582d402caaf0fa03694b0d0
Bachoo <cosmin_cb@hotmail.com>
2019-03-15 21:47:08 -0400

    updated the countdown

--- commands/countdown.js
+++ commands/countdown.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'countdown',
     execute(message, Discord, client) {
-       message.channel.send("soon:tm:");
+       message.channel.send("***very*** soon:tm:");
     }
 }
