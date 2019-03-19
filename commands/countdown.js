41c7b5a0f7692b61a86020102d3c8eb29532f24c
Bachoo <cosmin_cb@hotmail.com>
2019-03-18 21:01:46 -0400

    made countdown command cooler:tm:

--- commands/countdown.js
+++ commands/countdown.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'countdown',
     execute(message, Discord, client) {
-       message.channel.send("***very*** soon:tm:");
+       message.channel.send("***Very*** soon:tm:", {files: ["https://cdn.discordapp.com/attachments/421800263167246341/557218010360709120/Naamloos.png"]});
     }
 }
