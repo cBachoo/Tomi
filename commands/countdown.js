f859519c4dfea9bb3df31def4c2ad01059b34eb8
Bachoo <cosmin_cb@hotmail.com>
2019-03-20 01:36:51 -0400

    thing

--- commands/countdown.js
+++ commands/countdown.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'countdown',
     execute(message, Discord, client) {
-       message.channel.send("***Very*** soon:tm:", {files: ["https://cdn.discordapp.com/attachments/421800263167246341/557218010360709120/Naamloos.png"]});
+        message.channel.send("***N O W ***");
     }
 }
