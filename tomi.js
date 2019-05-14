231866f34755f452d5c0936e41294ac6dbf7ef2e
Bachoo <cosmin_cb@hotmail.com>
2019-05-13 21:22:19 -0400

    final commit to fix everything

--- tomi.js
+++ tomi.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const { Client, Collection } = require('discord.js');
-const config = require("./config.json");
+//const config = require("./config.json");
 
 const client = new Client();
 client.config = config;
