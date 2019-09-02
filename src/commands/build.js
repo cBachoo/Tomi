0236480422a422aa892c3ae172a390f20d9f4b22
Bachoo <cosmin_cb@hotmail.com>
2019-09-02 19:44:05 -0400

    made wine command better, added build command

---
+++ src/commands/build.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'build',
+    execute(client, message, raw) {
+        
+        message.channel.send("A good starting build is to go the `Awe Robe` + `Vampire's Eyeglasses` and a high DPS basic arcana")
+    }
+}
--- src/commands/wine.js
+++ src/commands/wine.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const stdin = fs.readFileSync('./src/misc/wine.txt');
+const winetxt = stdin.toString();
 module.exports = {
     name: 'wine',
     execute(client, message, raw){
-        message.channel.send("`Combine the yeast, sugar and juice concentrate in a gallon jug. Fill the jug the rest of the way with cold water. Rinse out a large balloon, and fit it over the opening of the jug... Then place jug in a cool dark place. Within a day you will notice the balloon starting to expand. And then let the god do the work.` \n\n:wine_glass: :wine_glass: :wine_glass: ")
+        message.channel.send(winetxt);
     }
 }
