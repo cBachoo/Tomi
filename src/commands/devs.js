5149334d8d0acbb7fd2655cfc5278fee12a65a9f
Bachoo <cosmin_cb@hotmail.com>
2019-09-30 21:15:27 -0400

    added new dev only command

---
+++ src/commands/devs.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'devs',
+    execute(client, message, raw) {
+
+        if (message.author.id === "236169260026560522" || message.author.id === "421801203970736128" ){
+            message.channel.send("You're very cool, and I appreciate you creating me :heart:");
+        } else {
+            console.log("fake")
+        }
+    }
+}
