8bfd05ed97861f6d82c8108a5afe4ffc3bf5acab
Bachoo <cosmin_cb@hotmail.com>
2019-11-04 19:23:12 -0500

    added some more fun options to previous commit command

--- src/commands/advice.js
+++ src/commands/advice.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: 'advice', 
     execute(client, message, raw) {
-        message.channel.send("Just dont get hit <:taffy:441425555787874314>");
+        var options = ["Just dont get hit", "Damage the enemy a lot", "Dont die", "doing damage from a distance makes sure you can do damage from a distance", "dying means that you lose", "if the enemy kills you, you die", "the mitochondria is the powerhouse of the cell", "eat food to not starve", "make sure your hp does not go down to 0" ]
+        //Math.floor(Math.random() * options.length)
+        message.channel.send(options[Math.floor(Math.random() * options.length)] + " <:taffy:441425555787874314>");
     }
 }
