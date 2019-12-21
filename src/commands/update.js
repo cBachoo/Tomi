91c28bd5420b08df4f87aec2c6588b9aaddeb578
Bachoo <cosmin_cb@hotmail.com>
2019-12-20 23:17:14 -0500

    updated update command

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'update',
     execute(client, message, raw) { 
-        message.channel.send("Soon:tm:")
+        message.channel.send("Soon:tm: \n\nhttps://steamcommunity.com/gid/103582791441657041/announcements/detail/1687093048755174939")
     }
 }
