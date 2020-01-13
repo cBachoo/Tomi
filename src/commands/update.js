f02b103de9be94441a18eb7fab8699dda3ea7410
Bachoo <cosmin_cb@hotmail.com>
2020-01-12 19:34:16 -0500

    removed embed from update command

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'update',
     execute(client, message, raw) { 
-        message.channel.send("Soon:tm: \n\nhttps://steamcommunity.com/gid/103582791441657041/announcements/detail/1687093048755174939")
+        message.channel.send("Soon:tm: \n\n<https://steamcommunity.com/gid/103582791441657041/announcements/detail/1687093048755174939>")
     }
 }
