b98f99d50384cdfb8fe33ed6ff8dee4a8382dd8f
Bachoo <cosmin_cb@hotmail.com>
2020-01-20 12:42:13 -0500

    smal QOL change for new robes to match colors

--- src/commands/avarice.js
+++ src/commands/avarice.js
@@ -5,7 +5,7 @@ module.exports = {
     execute(client, message, raw){
         var img = "https://gamepedia.cursecdn.com/wizardoflegend_gamepedia_en/5/52/Avarice.png?version=49a8a9d99ec0ac73443350fc37c2f005"
         var embed = new RichEmbed()
-            .setColor("ac65a5") 
+            .setColor("a59e97") 
             .setFooter("Tomi developed and maintained by Bachoo#0001")
             .setDescription("+15% Gold gain, +1% gem gain")
             .setThumbnail(img)
--- src/commands/level.js
+++ src/commands/level.js
@@ -5,7 +5,7 @@ module.exports = {
     execute(client, message, raw){
         var img = "https://gamepedia.cursecdn.com/wizardoflegend_gamepedia_en/3/3d/Level.png?version=f42eb63d86cce4089aa2184416bdfda4"
         var embed = new RichEmbed()
-            .setColor("ac65a5") 
+            .setColor("49486c") 
             .setFooter("Tomi developed and maintained by Bachoo#0001")
             .setDescription("-50% Max health, -50% Damage, -50% Signature charge rate. Each enemy you defeat increases the stats of the robe, up to a maximum of +50% of the former negative stats. With saville upgrade, the maximum is increases to +75% of the former negative stats!!!")
             .setThumbnail(img)
