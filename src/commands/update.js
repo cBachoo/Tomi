1f7d4c1c7d0e960466e656b0b739d5138efbbe87
Bachoo <cosmin_cb@hotmail.com>
2020-05-23 09:51:42 -0400

    update command done did

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'update',
     execute(client, message, raw) { 
-        message.channel.send("**Now**:tm:")
+        message.channel.send("https://cdn.discordapp.com/attachments/421800263167246341/713732116621426779/IllTellYou.gif")
     }
 }
