64d6b8adc5765d4b61304af55ffbcb5f22b61b74
Bachoo <cosmin_cb@hotmail.com>
2021-03-20 12:50:11 -0400

    new command added for 'tutorial glitch'

---
+++ src/commands/tutorial.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'bruh',
+    execute(client, message, raw) {
+        message.channel.send(`1. Replay tutorial via mirror NPC in your house\n2. You’ll arrive in your house and will be able to drop relics, so you can equip up to 12 from the start \n3. Go nuts <:taffy:441425555787874314>`);
+    }
+}
