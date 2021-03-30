772dd908a437b1d00a196645afe82dd856d95327
Bachoo <cosmin_cb@hotmail.com>
2021-03-30 14:05:49 -0400

    new command added

---
+++ src/commands/helpful/easymod.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'easymod',
+    execute(client, message, raw) {
+        message.channel.send(`
+        This is not how you add robes and relics and stuff, this is for bumping numbers and editing what spawns when.
+        \`\`\`
+        1. Download DnSpy : https://github.com/dnSpy/dnSpy/releases
+        2. Open Local Files via Steam
+        3. Find the Assembly-CSharp.dll file
+        4. Drag and Drop the dll File onto the Sidebar on Spy
+        5. You now have Access to the Game files! 
+        6. The Arcana and Relics are in under the {} - Directory
+        7. To Edit the Code, Simply right click and Select Edit Class or Edit Method\`\`\``)
+    }
+}
